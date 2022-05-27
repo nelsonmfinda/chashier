edb48a78a140ff49eaaef8ed7c59a1579d4ff5bd
Nelson Mfinda <hi@nelsonmfinda.dev>
2022-05-27 11:07:52 +0100

    Implement tests for the checkout feature

--- spec/cashier_spec.rb
+++ spec/cashier_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Cashier do
     expect(Cashier::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
 end
